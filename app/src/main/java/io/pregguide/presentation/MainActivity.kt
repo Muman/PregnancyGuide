681968748692c2d44ede99a025df3df5a68fdd34
Piotr Muciek <piotrekmuciek@gmail.com>
2021-07-25 21:38:19 +0200

    Add main activity

---
+++ app/src/main/java/io/pregguide/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.pregguide.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.pregguide.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
